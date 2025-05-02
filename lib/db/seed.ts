6f7e2d1957c4812cd941d439dcebc9af2e0fe9f4
Victor Ross <victor2ross@gmail.com>
2025-05-01 23:39:03 -0300

    05-connect-to-mongodb-and-seed-products

---
+++ lib/db/seed.ts
@@ -0,0 +1,31 @@
+"server-only";
+
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
+
+import data from "@/lib/data";
+import { connectToDatabase } from "@/lib/db";
+import Product from "./models/product.model";
+
+loadEnvConfig(cwd());
+
+const main = async () => {
+  try {
+    const { products } = data;
+
+    await connectToDatabase(process.env.MONGODB_URI);
+
+    await Product.deleteMany();
+    const createdProducts = await Product.insertMany(products);
+
+    console.log(`Created ${createdProducts.length} products.`);
+
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+
+    throw new Error("Failed to seed database.");
+  }
+};
+
+main();
---
+++ src/lib/db/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const cached: {
+  conn: mongoose.Mongoose | null;
+  promise: Promise<mongoose.Mongoose> | null;
+} = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async (
+  MONGODB_URI = process.env.MONGODB_URI
+) => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+  }
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI);
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
